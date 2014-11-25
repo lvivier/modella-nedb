73e4fe13ca00ce4b313d3ee2610870965979d7d8
Luke Vivier <luke@vivier.ca>
2014-11-24 22:45:50 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,109 @@
+
+# modella-nedb
+
+[NeDB][ne] persistence for modella.
+
+NeDB is a tiny embedded database, written in JavaScript, with a
+MongoDB-like API. It's ideal for prototyping, microservices, and
+desktop/CLI applications, and has a human-readable file format.
+
+
+## Install
+
+    $ npm install modella-nedb
+
+
+## Example
+
+```js
+var nedb = require('modella-nedb')('./user.db')
+var model = require('modella')
+
+var User = model('User')
+  .attr('_id')
+  .attr('name')
+  .attr('email', {unique:true})
+  .attr('age')
+
+User.use(nedb)
+
+// query
+User
+  .query({age:{$gt:10}})
+  .sort({age:-1})
+  .exec(function(err, users){
+    // users is an array of User instances
+  })
+```
+
+
+## API
+
+### Model#save(cb)
+
+Saves a model's properties to disk and calls `cb`.
+
+### Model#remove(cb)
+
+Removes a model from the db.
+
+### Model.find(query, cb)
+
+Aliased to `Model.get`. Finds a single model using `query`.
+
+### Model.all(query[, cb])
+
+Aliased to `Model.query`. Finds models using `query`, calls `cb` or returns a Cursor (see below).
+
+### Model.removeAll(query, cb)
+
+Remove models matching `query` and calls `cb`.
+
+### Model.index(opts[, cb])
+
+Set an index on the datastore. Options are:
+
+- **fieldName** Name of the field to index
+- **unique** Enforce uniqueness
+- **sparse** Don't index documents that don't have this field
+
+You can also specify indexes in the model attributes, like this:
+
+```js
+User
+  .attr('age', {index:true})    // indexed
+  .attr('email', {unique:true}) // unique
+```
+
+### Model.db
+
+NeDB `Datastore` instance. Use for direct database access.
+
+### Cursor
+
+A call to `Model.all`/`Model.query` without a callback function
+returns a `Cursor` instance. This is the same as an NeDB cursor, but
+it will respond with an array of `Model` instances.
+
+#### Cursor#sort(fields)
+
+Sort the result set by an object of `fields` like `{email:1, age:-1}`.
+
+#### Cursor#skip(n)
+
+Skip `n` records in the result set.
+
+#### Cursor#limit(n)
+
+Limit the result set to `n` records.
+
+#### Cursor#exec(fn)
+
+Execute the query and call `fn` with the results.
+
+
+## License
+
+MIT
+
+[ne]:https://github.com/louischatriot/nedb
---
+++ test/index.js
@@ -0,0 +1,145 @@
+
+var Cursor = require('nedb/lib/cursor')
+var Datastore = require('nedb')
+var assert = require('assert')
+var model = require('modella')
+var nedb = require('..')
+
+/**
+ * Database path
+ */
+
+var USER_PATH = __dirname + '/user.db'
+
+
+/**
+ * User model
+ */
+
+var User = model('User')
+  .attr('_id')
+  .attr('name', {type:String})
+  .attr('email', {unique:true})
+  .attr('createdAt', {type:Date})
+  .use(nedb(USER_PATH))
+
+
+/**
+ * Fixture
+ */
+
+var obj = {
+  name: 'Elvis',
+  email: 'elvis@derp.ie',
+  createdAt: new Date()
+}
+
+var user = new User(obj)
+
+
+/**
+ * Tests
+ */
+
+suite('Interface')
+
+test('Sync layer methods exist', function(){
+  assert('function' === typeof User.save)
+  assert('function' === typeof User.update)
+  assert('function' === typeof User.remove)
+})
+
+test('Datastore exposed at Model.db', function(){
+  assert(User.db instanceof Datastore)
+})
+
+suite('Model.save')
+
+test('Saves a model', function(done){
+  user.save(done)
+})
+
+test('Respects unique index', function(done){
+  var user2 = new User({name:'Jerry', email:'elvis@derp.ie'})
+  user2.save(function(err){
+    assert.equal('uniqueViolated', err.errorType)
+    assert.equal('elvis@derp.ie', err.key)
+    done()
+  })
+})
+
+suite('Model.update')
+
+test('Updates a document', function(done){
+  user.name('Ronald')
+  user.save(done)
+})
+
+test('No changes made', function(done){
+  user.save(done)
+})
+
+suite('Model.find')
+
+test('Aliased to Model.get', function(){
+  assert.equal(User.get, User.find)
+})
+
+test('Finds one by id', function(done){
+  User.find(user.primary(), function(err, user){
+    assert(user instanceof User)
+    assert.equal('Ronald', user.name())
+    assert.equal('elvis@derp.ie', user.email())
+    done(err)
+  })
+})
+
+test('Finds one by query', function(done){
+  User.find({name:'Ronald'}, function(err, user){
+    assert(user instanceof User)
+    done(err)
+  })
+})
+
+suite('Model.all')
+
+test('Aliased to Model.query', function(){
+  assert.equal(User.all, User.query)
+})
+
+test('Finds documents by query', function(done){
+  User.all({name:'Ronald'}, function(err, arr){
+    assert.equal(1, arr.length)
+    assert(arr[0] instanceof User)
+    done(err)
+  })
+})
+
+test('Returns a cursor', function(){
+  var cur = User.query({})
+  assert(cur instanceof Cursor)
+})
+
+test('Cursor gives array of model instances', function(done){
+  User.query({name:'Ronald'}).exec(function(err, arr){
+    assert(Array.isArray(arr))
+    assert.equal(1, arr.length)
+    assert(arr[0] instanceof User)
+    done(err)
+  })
+})
+
+suite('Model.count')
+
+test('Counts by query', function(done){
+  User.count({name:'Ronald'}, function(err, num){
+    assert.equal(1, num)
+    done(err)
+  })
+})
+
+suite('Model.remove')
+
+test('Removes a model', function(done){
+  user.remove(done)
+})
